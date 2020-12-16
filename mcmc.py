48a104f5655672b456c0eb846a0e0ec8547f0a4c
jaehyun_lee <leequant761@gmail.com>
2020-12-16 23:55:49 +0900

    Remove repeated computations

--- mcmc.py
+++ mcmc.py
@@ -47,12 +47,16 @@ def HMC(state, step_size, num_step):
     h_state['p'] = -(h_state['p'] + step_size/2 * grad_log_posterior(h_state))
     h_state['w'] = np.exp(h_state['log_w'])
 
+    # to avoid repeated computation
+    w_sum = state['w'].sum()
+    wprop_sum= h_state['w'].sum()
+
     # Accept the proposal with probability with min(1,r)
     term1 = (state['m'] - state['sigma']) * (h_state['log_w'] - np.log(state['w']))
     term1 = term1.sum()
-    term2 = -(h_state['w'].sum() + state['w_star'])**2
-    term3 = (state['w'].sum() + state['w_star'])**2
-    term4 = -state['tau'] * (h_state['w'].sum() - state['w'].sum()) # paper errata
+    term2 = -(wprop_sum + state['w_star'])**2
+    term3 = (w_sum + state['w_star'])**2
+    term4 = -state['tau'] * (wprop_sum - w_sum) # paper errata
     term5 = -1/2 * (h_state['p']**2-init_p**2).sum()
     log_r = term1 + term2 + term3 + term4 + term5
 
@@ -70,12 +74,15 @@ def MH(state, sigma_tau):
     ----------
     (Caron, 2015) E.1. Step 2
     """
+    # to avoid repeated computation
+    w_sum = state['w'].sum()
+
     N_alpha = len(state['w'])
     m_state = {
         'tau': lognormal(log(state['tau']), sigma_tau),
         'sigma': 1-lognormal(log(1-state['sigma']), sigma_tau),
     }
-    freq_term1 = m_state['tau'] + 2*state['w'].sum() + state['w_star']
+    freq_term1 = m_state['tau'] + 2*w_sum + state['w_star']
     m_state['alpha'] = gamma(
         N_alpha, 
         m_state['sigma'] / (freq_term1**m_state['sigma'] - state['tau']**m_state['sigma'])
@@ -85,19 +92,19 @@ def MH(state, sigma_tau):
                                             m_state['sigma'], freq_term1, 1)[0]
     else:
         m_state['w_star'] = finite_crm_Caron(m_state['alpha'], m_state['sigma'], 
-                                m_state['tau']+2*state['w'].sum()+state['w_star'])
+                                m_state['tau']+2*w_sum+state['w_star'])
     
-    # freq_term2 = state['tau'] + 2*sum(state['w']) + m_state['w_star']
+    # freq_term2 = state['tau'] + 2*w_sum + m_state['w_star']
 
     # compute acceptance probability
-    term1 = (sum(state['w']) + state['w_star'])**2 - (sum(state['w']) + m_state['w_star'])**2
-    term2 = -(m_state['tau']-state['tau']-2*m_state['w_star']+2*state['w_star']) * state['w'].sum()
+    term1 = (w_sum + state['w_star'])**2 - (w_sum + m_state['w_star'])**2
+    term2 = -(m_state['tau']-state['tau']-2*m_state['w_star']+2*state['w_star']) * w_sum
     term3 = (-m_state['sigma']+state['sigma']) * log(state['w']).sum()
     
     # term4's numerator
     term4_n1 = log(GAMMA(1-state['sigma']))
     # term4_n2 = log((freq_term2**state['sigma'] - state['tau']**state['sigma']) / state['sigma'])
-    stable_term2 = 1 + (2*state['w'].sum() + m_state['w_star']) / state['tau']
+    stable_term2 = 1 + (2*w_sum + m_state['w_star']) / state['tau']
     stable_term2 = (stable_term2 ** state['sigma'] - 1) / state['sigma']
     term4_n2 = log(stable_term2) + state['sigma'] * log(state['tau'])
     term4_n = term4_n1 + term4_n2
@@ -105,7 +112,7 @@ def MH(state, sigma_tau):
     # term4's denominator
     term4_d1 = log(GAMMA(1-m_state['sigma']))
     # term4_d2 = log(freq_term1**m_state['sigma'] - m_state['tau']**m_state['sigma'])
-    stable_term1 = 1 + (2*state['w'].sum() + state['w_star']) / m_state['tau']
+    stable_term1 = 1 + (2*w_sum + state['w_star']) / m_state['tau']
     stable_term1 = (stable_term1 ** m_state['sigma'] - 1) / m_state['sigma']
     term4_d2 = log(stable_term1) + m_state['sigma'] * log(m_state['tau'])
     term4_d = term4_d1 + term4_d2
