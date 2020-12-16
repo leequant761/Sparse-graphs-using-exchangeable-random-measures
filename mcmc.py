1. Fix my code errata 2. Stable code in numerical computation sense

mcmc.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 from numpy import log
 from numpy.random import lognormal, gamma
@@ -73,10 +75,10 @@ def MH(state, sigma_tau):
         'tau': lognormal(log(state['tau']), sigma_tau),
         'sigma': 1-lognormal(log(1-state['sigma']), sigma_tau),
     }
-    freq_term1 = m_state['tau'] + 2*sum(state['w']) + state['w_star']
+    freq_term1 = m_state['tau'] + 2*state['w'].sum() + state['w_star']
     m_state['alpha'] = gamma(
         N_alpha, 
-        (freq_term1**m_state['sigma'] - state['tau']**m_state['sigma']) / m_state['sigma']
+        m_state['sigma'] / (freq_term1**m_state['sigma'] - state['tau']**m_state['sigma'])
         )
     if m_state['sigma'] > 0:
         m_state['w_star'] = ets_sampling_Caron(m_state['alpha'], 
@@ -84,24 +86,34 @@ def MH(state, sigma_tau):
     else:
         m_state['w_star'] = finite_crm_Caron(m_state['alpha'], m_state['sigma'], 
                                 m_state['tau']+2*state['w'].sum()+state['w_star'])
-    freq_term2 = state['tau'] + 2*sum(state['w']) + m_state['w_star']
+    
+    # freq_term2 = state['tau'] + 2*sum(state['w']) + m_state['w_star']
 
     # compute acceptance probability
-    term1 = (sum(state['w']) + state['w_star'])**2 - (sum(state['w']) + state['w_star'])**2
-    term2 = -(m_state['tau']-state['tau']+2*state['w_star']-2*m_state['w_star']) * state['w'].sum()
+    term1 = (sum(state['w']) + state['w_star'])**2 - (sum(state['w']) + m_state['w_star'])**2
+    term2 = -(m_state['tau']-state['tau']-2*m_state['w_star']+2*state['w_star']) * state['w'].sum()
     term3 = (-m_state['sigma']+state['sigma']) * log(state['w']).sum()
-    term4_n1 = log(GAMMA(1-state['sigma'])) - log(state['sigma'])
-    term4_n2 = log(freq_term2**state['sigma'] - state['tau']**state['sigma'])
-    term4_n = term4_n1 + term4_n2
-    term4_d1 = log(GAMMA(1-m_state['sigma'])) - log(m_state['sigma'])
-    term4_d2 = log(freq_term1**m_state['sigma'] - m_state['tau']**m_state['sigma'])
-    term4_d = term4_d1 + term4_d2
-    term4 = N_alpha * (term4_n - term4_d)
-    r = np.exp(sum([term1, term2, term3, term4]))
-    acceptance_pbt = min(1, r)
-    is_accept = np.random.binomial(1, acceptance_pbt)
     
-    if is_accept:
+    # term4's numerator
+    term4_n1 = log(GAMMA(1-state['sigma']))
+    # term4_n2 = log((freq_term2**state['sigma'] - state['tau']**state['sigma']) / state['sigma'])
+    stable_term2 = 1 + (2*state['w'].sum() + m_state['w_star']) / state['tau']
+    stable_term2 = (stable_term2 ** state['sigma'] - 1) / state['sigma']
+    term4_n2 = log(stable_term2) + state['sigma'] * log(state['tau'])
+    term4_n = term4_n1 + term4_n2
+    
+    # term4's denominator
+    term4_d1 = log(GAMMA(1-m_state['sigma']))
+    # term4_d2 = log(freq_term1**m_state['sigma'] - m_state['tau']**m_state['sigma'])
+    stable_term1 = 1 + (2*state['w'].sum() + state['w_star']) / m_state['tau']
+    stable_term1 = (stable_term1 ** m_state['sigma'] - 1) / m_state['sigma']
+    term4_d2 = log(stable_term1) + m_state['sigma'] * log(m_state['tau'])
+    term4_d = term4_d1 + term4_d2
+
+    term4 = N_alpha * (term4_n - term4_d)
+    
+    log_r = term1 + term2 + term3 + term4
+    if log(random.random()) < log_r:
         state['alpha'] = m_state['alpha']
         state['sigma'] = m_state['sigma']
         state['tau'] = m_state['tau']